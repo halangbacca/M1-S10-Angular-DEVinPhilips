f4a702f6b2f3dce5aa99fd28d1f5b7fb849e89e4
halangbacca <halanbacca@outlook.com>
2023-01-15 13:07:36 -0300

    router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './components/content/content.component';
@@ -9,6 +10,12 @@ import { FiltroComponent } from './components/filtro/filtro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'sobre', component: AboutComponent },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,12 +24,10 @@ import { AboutComponent } from './pages/about/about.component';
     FooterComponent,
     FiltroComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
   ],
-  imports: [
-    BrowserModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  providers: [AppComponent, HomeComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
