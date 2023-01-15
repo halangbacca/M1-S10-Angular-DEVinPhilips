1cb5430b1afb02e671fd762c5d214b1e97d94ca0
halangbacca <halanbacca@outlook.com>
2023-01-15 11:06:54 -0300

    EventEmitter

--- src/app/components/filtro/filtro.component.ts
+++ src/app/components/filtro/filtro.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'ntap-filtro',
   templateUrl: './filtro.component.html',
-  styleUrls: ['./filtro.component.scss']
+  styleUrls: ['./filtro.component.scss'],
 })
 export class FiltroComponent {
+  lista: string[] = ['Todos', 'Não lidos', 'Lidos'];
 
+  @Output() clique = new EventEmitter<string>();
+
+  onClick(event: any) {
+    this.clique.emit(event);
+  }
 }
